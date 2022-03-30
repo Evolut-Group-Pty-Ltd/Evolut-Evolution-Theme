2a73583eba0c5845bd11141a59cc63be76d7a4d6
evolut-group <ben@evolut.com.au>
2022-03-31 07:42:22 +1000

    Update header.php
    Manually set page background color and color via ACF

--- partials/header/header.php
+++ partials/header/header.php
@@ -2,4 +2,4 @@
 get_template_part('partials/header/header', 'minimal');
 get_template_part('partials/header/header', 'body', $args);
 ?>
-<main class="main">
+<main class="main"<?php echo (get_field("page_background_colour") || get_field("page_type_colour")) ? sprintf(' style="background-color: %s; color: %s;"', get_field("page_background_colour"), get_field("page_type_colour")) : '' ?>>
