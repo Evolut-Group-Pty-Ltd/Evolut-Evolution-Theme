e96adc1b7b1fe6c0cb357a2647a1b0f55582e0f2
evolut-group <ben@evolut.com.au>
2022-03-30 23:34:41 +1000

    Update block.php

--- blocks/table/block.php
+++ blocks/table/block.php
@@ -1,23 +1,4 @@
-<?php
-get_template_part('blocks/block-wrapper', null, array(
-  'classes' => !empty($block['className']) ? $block['className'] : '',
-  'title' => get_field('title'),
-  'heading_tag' => get_field('heading_tag'),
-  'alignment' => get_field('alignment'),
-  'sub_heading' => get_field('sub_heading'),
-  'block_links' => get_field('block_links'),
-  'description' => get_field('description'),
-  'margin_formatting' => get_field('margin_formatting'),
-  'padding' => get_field('padding'),
-  'background_image' => get_field('background_image'),
-  'bg_image_settings' => get_field('bg_image_settings'),
-  'background_video' => get_field('background_video'),
-  'minimum_height' => get_field('minimum_height'),
-  'heading_position' => get_field('heading_position'),
-  'block_unique_id' => get_field('block_unique_id'),
-  'block_name' => 'table',
-));
-?>
+<?php get_template_part('blocks/acf-block-inputs', null, ['block_name' => 'table']); ?>
 <?php
 get_template_part('blocks/table/partial', null, array(
   'classes' => !empty($block['className']) ? $block['className'] : '',
@@ -25,15 +6,4 @@ get_template_part('blocks/table/partial', null, array(
   'table' => get_field('table'),
 ));
 ?>
-<?php
-get_template_part('blocks/block-wrapper-end', null, array(
-  'classes' => !empty($block['className']) ? $block['className'] : '',
-  'title' => get_field('title'),
-  'heading_tag' => get_field('heading_tag'),
-  'alignment' => get_field('alignment'),
-  'sub_heading' => get_field('sub_heading'),
-  'block_links' => get_field('block_links'),
-  'description' => get_field('description'),
-  'heading_position' => get_field('heading_position'),
-));
-?>
+<?php get_template_part('blocks/acf-block-inputs', null, ['position' => 'end']); ?>
